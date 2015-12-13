4942693f10c39c072631ce90e61eee08057ef353
J. Heasly <jheasly@earthlink.net>
2015-12-13 13:58:34 -0800

    Initial commit.

---
+++ scraper.py
@@ -0,0 +1,20 @@
+# https://github.com/ireapps/first-web-scraper/blob/master/scrapers/crime/scrape.py
+import csv
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url = 'http://www.footballoutsiders.com/stats/fplus'
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html)
+tbody = soup.find('tbody')
+
+list_of_rows = []
+for row in tbody.findAll('tr'):
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        list_of_cells.append(cell.text)
+    list_of_rows.append(list_of_cells)
+
+print list_of_rows
